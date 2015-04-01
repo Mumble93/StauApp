1ea98f6765ed3f20f6ded515e07b30119802a0a4
Jan Paul <jan.paul1@gmx.net>
2015-04-01 21:48:53 +0200

    Implemented basics for GCM Message receiving

--- StauApp/app/src/main/java/com/sta/dhbw/stauapp/MainActivity.java
+++ StauApp/app/src/main/java/com/sta/dhbw/stauapp/MainActivity.java
@@ -1,54 +1,215 @@
 package com.sta.dhbw.stauapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class MainActivity extends ActionBarActivity
 {
     private boolean gpsIsActive;
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    public static final String PROPERTY_REG_ID = "registration_id";
+    private static final String PROPERTY_APP_VERSION = "appVersion";
 
+    private static final String TAG = "JamBeacon";
+
+    String SENDER_ID = "821661182636";
+
+    TextView mDisplay;
+    GoogleCloudMessaging gcm;
+    AtomicInteger msgId = new AtomicInteger();
+    SharedPreferences prefs;
+    Context context;
+
+    String regId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDisplay = (TextView) findViewById(R.id.message_display);
+
+        context = getApplicationContext();
+
+        //Check for Play Services APK. Proceed with GCM registration, if successful
+        if (checkPlayServices())
+        {
+            gcm = GoogleCloudMessaging.getInstance(this);
+            regId = getRegistrationId(context);
+
+            if (regId.isEmpty())
+            {
+                registerInBackground();
+            } else
+            {
+                Log.i(TAG, "No valid Google Play Services APK found.");
+            }
+        }
     }
 
     @Override
     protected void onResume()
     {
         super.onResume();
-        gpsIsActive = Utils.checkGps(MainActivity.this);
+        gpsIsActive = Utils.checkGps(context);
+        checkPlayServices();
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
+    /**
+     * Checks if a valid Google Play Services APK is available
+     *
+     * @return TRUE if APK is available, FALSE otherwise
+     */
+    private boolean checkPlayServices()
     {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS)
+        {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode))
+            {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else
+            {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
+
+    /**
+     * Get the current GCM registration ID
+     * <p/>
+     * If the returned String is empty, the device needs to register.
+     *
+     * @param context The application context
+     * @return The registration ID, if it exists. If not, an empty String is returned.
+     */
+    private String getRegistrationId(Context context)
     {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
+        final SharedPreferences prefs = getGCMPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty())
         {
-            return true;
+            Log.i(TAG, "Registration not found.");
+            return "";
         }
-
-        return super.onOptionsItemSelected(item);
+        //Check for app updates
+        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion)
+        {
+            Log.i(TAG, "App version changed");
+            return "";
+        }
+        return registrationId;
     }
 
+    /**
+     * @return The application's {@code SharedPreferences}
+     */
+    private SharedPreferences getGCMPreferences(Context context)
+    {
+        return getSharedPreferences(MainActivity.class.getSimpleName(),
+                Context.MODE_PRIVATE);
+    }
+
+    /**
+     * @return Application's version code from the {@code PackageManager}.
+     */
+    private static int getAppVersion(Context context)
+    {
+        try
+        {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e)
+        {
+            //should never happen. If so, your programming is bad, and you should feel bad.
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+
+    private void registerInBackground()
+    {
+        new AsyncTask<Void, Void, String>()
+        {
+            @Override
+            protected String doInBackground(Void... params)
+            {
+                String msg = "";
+                try
+                {
+                    if (gcm == null)
+                    {
+                        gcm = GoogleCloudMessaging.getInstance(context);
+                    }
+                    regId = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID = " + regId;
+                    //Send Id to server, so server can send messages to app
+                    sendRegistrationIdToBackend();
+                    //Persist registration Id
+                    storeRegistrationId(context, regId);
+                } catch (IOException ex)
+                {
+                    msg = "Error: " + ex.getMessage();
+                    //ToDo: Implement mechanism to prompt user to re-register
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg)
+            {
+                mDisplay.append(msg + "\n");
+            }
+        }.execute(null, null, null);
+    }
+
+    /**
+     * Send registration Id to application server, so server can send messages to device.
+     */
+    private void sendRegistrationIdToBackend()
+    {
+        //ToDo: I don't think it is necessary to do this, still keeping it here just in case.
+    }
+
+    /**
+     * Stores the registration Id and appVersion Code in {@code SharedPreferences}.
+     *
+     * @param context The application's context
+     * @param regId   registration Id
+     */
+    private void storeRegistrationId(Context context, String regId)
+    {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        int appVersion = getAppVersion(context);
+        Log.i(TAG, "Saving regId on app version " + appVersion);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.apply();
+    }
 }
+
--- StauApp/app/src/main/java/com/sta/dhbw/stauapp/SplashScreen.java
+++ StauApp/app/src/main/java/com/sta/dhbw/stauapp/SplashScreen.java
@@ -3,6 +3,7 @@ package com.sta.dhbw.stauapp;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -10,13 +11,18 @@ import android.os.Bundle;
 
 public class SplashScreen extends Activity
 {
+    Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-        if (Utils.checkGps(SplashScreen.this))
+
+        context = getApplicationContext();
+
+        Intent intent = new Intent(context, MainActivity.class);
+        if (Utils.checkGps(context))
         {
             //ToDo: Implement check for server connectivity and retrieval of known traffic issues
             try
