da8f1f3a925fbac7cf98014ec9bc32678ce7cbac
Jan Paul <jan.paul1@gmx.net>
2015-05-08 16:14:05 +0200

    Implemented Serialization and Deserialization for TrafficJam object

--- StauApp/app/src/main/java/com/sta/dhbw/stauapp/jam/TrafficJamDeserializer.java
+++ StauApp/app/src/main/java/com/sta/dhbw/stauapp/jam/TrafficJamDeserializer.java
@@ -1,5 +1,44 @@
 package com.sta.dhbw.stauapp.jam;
 
-public class TrafficJamDeserializer
+import android.location.Location;
+import android.location.LocationManager;
+import android.util.Log;
+
+import com.sta.dhbw.stauapp.exception.JamBeaconException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.UUID;
+
+public final class TrafficJamDeserializer
 {
+    private static final String TAG = TrafficJamDeserializer.class.getSimpleName();
+
+    public static TrafficJam jsonToTrafficJam(JSONObject jsonObject) throws JamBeaconException
+    {
+        Location location = new Location(LocationManager.GPS_PROVIDER);
+        long timestamp;
+        UUID uuid;
+
+        try
+        {
+            JSONObject locationJson = jsonObject.getJSONObject("location");
+            double longitude = locationJson.getDouble("longitude");
+            double latitude = locationJson.getDouble("latitude");
+            location.setLatitude(latitude);
+            location.setLongitude(longitude);
+
+            timestamp = jsonObject.getLong("timestamp");
+
+            uuid = UUID.fromString(jsonObject.getString("id"));
+
+        } catch (JSONException je)
+        {
+            Log.e(TAG, "Error deserializing response");
+            throw new JamBeaconException(je.getMessage(), je);
+        }
+
+        return new TrafficJam(location, timestamp, uuid);
+    }
 }
--- StauApp/app/src/main/java/com/sta/dhbw/stauapp/jam/TrafficJamSerializer.java
+++ StauApp/app/src/main/java/com/sta/dhbw/stauapp/jam/TrafficJamSerializer.java
@@ -1,5 +1,34 @@
 package com.sta.dhbw.stauapp.jam;
 
-public class TrafficJamSerializer
+import android.util.Log;
+
+import com.sta.dhbw.stauapp.exception.JamBeaconException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public final class TrafficJamSerializer
 {
+    private static final String TAG = TrafficJamSerializer.class.getSimpleName();
+
+    public static JSONObject trafficJamToJson(TrafficJam trafficJam) throws JamBeaconException
+    {
+        JSONObject jamJson = new JSONObject();
+        JSONObject locationObject = new JSONObject();
+        try
+        {
+            locationObject.put("longitude", trafficJam.getLocation().getLongitude());
+            locationObject.put("latitude", trafficJam.getLocation().getLatitude());
+
+            jamJson.put("location",locationObject);
+            jamJson.put("timestamp", trafficJam.getTimestamp());
+            jamJson.put("id", trafficJam.getId().toString());
+        }catch (JSONException je)
+        {
+            Log.e(TAG, "Error serializing jam");
+            throw new JamBeaconException(je.getMessage(), je);
+        }
+
+        return jamJson;
+    }
 }
