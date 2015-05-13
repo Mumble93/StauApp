b2fab9b7844000f5c8ba03a92b8607d8e67cbbda
Jan Paul <jan.paul1@gmx.net>
2015-05-13 11:31:31 +0200

    Relocated files to other packages

--- StauServer/jambeacon-stauserver/src/main/java/com/sta/dhbw/stauserver/JamServerApplication.java
+++
@@ -1,9 +0,0 @@
-package com.sta.dhbw.stauserver;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api/v1")
-public class JamServerApplication extends Application
-{
-}
